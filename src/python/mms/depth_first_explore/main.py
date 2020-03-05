d6683bf0a8870a59d55c6be72b55cb4bf9499a7a
vinnyputty <vinayputreddi@gmail.com>
2020-03-04 18:17:49 -0800

    Refactor and add TODO for configurable optimizations

--- src/python/mms/depth_first_explore/main.py
+++ src/python/mms/depth_first_explore/main.py
@@ -84,14 +84,19 @@ def update_position(current_position, direction_marker) -> (int, int):
 # FIXME handling of endless loop of explores due to:
 #   - an explore that returns to the starting cell of that explore
 #   - any other reason for and endless loop of explores
-# TODO implement "return to start" when below conditions are met:
-#   - every cell representation is known
-#   - every cell representation but the goal cells is known
-#   - unvisited list is empty
-#   - unvisited list only contains goal cells
 # TODO implement "target largest unvisited regions"
 # TODO implement "target exploration of particular cell"
 #   - this might require a known path to a cell adjacent to that particular cell
+# TODO implement configurable optimizations
+#   - a single unit width path with at least two known neighboring cells for each cell on the path can be assumed to be
+#   known
+#   - "return to start" when below conditions are met ("return to start" includes both calculating route, cancelling
+#   current route, and following calculated route):
+#       - every cell representation is known
+#       - every cell representation but the goal cells is known
+#       - unvisited list is empty
+#       - unvisited list only contains goal cells
+
 # a depth-first exploration of the maze
 def explore(current_position, direction_marker, maze_state=None, unvisited_list: list = None, explore_depth=0):
     explore_depth += 1
